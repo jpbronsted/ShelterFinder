c3f2fc5e9f15dbb70c6711068ece24367b920e0b
James Bronsted <jamespbronsted@gmail.com>
2018-02-13 20:26:58 -0500

    Initial Commit

---
+++ app/src/main/java/edu/gatech/team13/shelterfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.team13.shelterfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
