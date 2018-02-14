63806d95beadaef7cb9b2e0d45a87d7931cb71c0
James Bronsted <jamespbronsted@gmail.com>
2018-02-13 23:38:01 -0500

    James - created two new views (Welcome and Login screens)

--- app/src/main/java/edu/gatech/team13/shelterfinder/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/team13/shelterfinder/WelcomeActivity.java
@@ -3,11 +3,13 @@ package edu.gatech.team13.shelterfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
+
 }
