38d6401251e26a9bd203949d65ba1112d045ccad
nowal <waldronnoah@gmail.com>
2018-04-04 01:48:26 -0400

    Noah- made MapActivity and activity_map

---
+++ app/src/main/java/controllers/MapActivity.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import team.gatech.edu.login.R;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+    }
+}
